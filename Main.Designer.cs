edda7ca7a7ccb8c9c5dff43f17471c164b46579b
jason.the.human2005 <jason.the.human2005@gmail.com>
2021-08-14 14:18:12 +0100

    Add project files.

---
+++ Main.Designer.cs
@@ -0,0 +1,73 @@
+﻿
+namespace MineSharp
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.startServerButton = new System.Windows.Forms.Button();
+            this.stopServerButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // startServerButton
+            // 
+            this.startServerButton.Location = new System.Drawing.Point(12, 12);
+            this.startServerButton.Name = "startServerButton";
+            this.startServerButton.Size = new System.Drawing.Size(73, 27);
+            this.startServerButton.TabIndex = 0;
+            this.startServerButton.Text = "Start";
+            this.startServerButton.UseVisualStyleBackColor = true;
+            // 
+            // stopServerButton
+            // 
+            this.stopServerButton.Location = new System.Drawing.Point(12, 45);
+            this.stopServerButton.Name = "stopServerButton";
+            this.stopServerButton.Size = new System.Drawing.Size(73, 27);
+            this.stopServerButton.TabIndex = 1;
+            this.stopServerButton.Text = "Stop";
+            this.stopServerButton.UseVisualStyleBackColor = true;
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.stopServerButton);
+            this.Controls.Add(this.startServerButton);
+            this.Name = "Main";
+            this.Text = "MineSharp";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button startServerButton;
+        private System.Windows.Forms.Button stopServerButton;
+    }
+}
+
